abfd7338e8c451956b9a746878481bf0848b3060
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-01-12 23:26:26 -0500

    Flatten

--- src/recursion.js
+++ src/recursion.js
@@ -223,6 +223,19 @@ var nestedEvenSum = function(obj) {
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+
+  var result = [];
+  // Base case
+  if (!Array.isArray(array)) {
+    return array;
+  }
+
+  // Recursive case
+  array.forEach(function(item) {
+    result = result.concat(flatten(item));
+  });
+
+  return result;
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
