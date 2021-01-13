c2526e284cf4023a83a9809649c448f896e8ccf2
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-01-12 23:53:57 -0500

    Exponents (7) w/o optimization

--- src/recursion.js
+++ src/recursion.js
@@ -72,6 +72,18 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  // Base case
+  if (exp === 0) {
+    return 1;
+  }
+
+  // Recursive case
+  if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  } else {
+    return 1 / (base * exponent(base, -exp - 1));
+  }
+
 };
 
 // 8. Determine if a number is a power of two.
