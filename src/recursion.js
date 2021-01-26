859a58e0426ba486955afb818ad8dc6ed21923c1
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-01-25 19:01:48 -0500

    Compare strings

--- src/recursion.js
+++ src/recursion.js
@@ -215,6 +215,11 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+
+  return str1[0] === str2[0] && compareStr(str1.slice(1),str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
