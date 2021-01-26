04acf681595d7745c33e731548fc863aa9ca2af6
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-01-25 19:18:10 -0500

    Multiply (12)

--- src/recursion.js
+++ src/recursion.js
@@ -194,7 +194,21 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
-
+  if (y === 0 || x === 0) {
+    return 0;
+  }
+  if (y > 0 && x > 0) {
+    return x + multiply(x, y - 1);
+  }
+  if (y > 0 && x < 0) {
+    return x + multiply(x, y - 1);
+  }
+  if (y < 0 && x > 0) {
+    return -x + multiply(x, y + 1);
+  }
+  if (y < 0 && x < 0) {
+    return -x + multiply(x, y + 1);
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
